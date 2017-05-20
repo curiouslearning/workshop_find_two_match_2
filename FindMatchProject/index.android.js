a56bde9ff514daf1e7f9aa893cfe219911f07a74
Deanna Heer <deannah@mit.edu>
2017-05-20 16:29:51 -0400

    Removing AwesomeProject and creating FindMatchProject.

---
+++ FindMatchProject/index.android.js
@@ -0,0 +1,12 @@
+/**
+ * FindMatchProject register app
+ */
+
+//import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import FindMatchProject from "./js/main";
+
+AppRegistry.registerComponent('FindMatchProject', () => FindMatchProject);
