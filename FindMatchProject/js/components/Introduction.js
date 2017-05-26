5701a634abeddef0ad9d5ddd881af8c5bb80850e
Deanna Heer <deannah@mit.edu>
2017-05-26 17:32:07 -0400

    Adjustments to main page.

--- FindMatchProject/js/components/Introduction.js
+++ FindMatchProject/js/components/Introduction.js
@@ -6,13 +6,15 @@ import React, {Component} from "react";
 import {
     StyleSheet,
     Text,
-    Button,
-    View
+    TouchableOpacity,
+    View,
+    Dimensions
 } from "react-native";
 
 import AnimatedSprite from "react-native-animated-sprite";
 import catSprite from "../sprites/cat/catSprite";
 
+const RightArrow = "\u27aa";
 const CONTENT = {
     trial_num: 0,
     left: [
@@ -51,7 +53,7 @@ const CONTENT = {
         {
             object_id: 6,
             target: "u",
-            pos: [0, 50],
+            pos: [50, 50],
             pair_id: [3]
         }
     ]
@@ -62,49 +64,50 @@ class Introduction extends Component {
         super();
         this.state = {
             animationType: "NORMAL",
-            text: "¡Hola, aprendamos español!"
+            text: "¡Hola, aprendamos español!",
+            buttonText: `¡Juguemos! ${RightArrow}`
         };
     }
 
     // toggles whether the cat is angry
     onPress () {
-        this.setState({ animationType: (this.state.animationType == "NORMAL" ? "ANGRY" : "NORMAL")});
+        this.refs.catRef.startTween();
     }
 
     render() {
         const {navigate} = this.props.navigation;
+        const screenSize = Dimensions.get("window");
+        const catY = screenSize.height - catSprite.size.height;
+        const catXMin = 50;
+        const catXMax = screenSize.width - catSprite.size.width - 50;
         return (
-            <View style={this.props.style}>
+            <View>
                 <Text style={styles.welcome}>
                     {this.state.text}
                 </Text>
-                <Button title="Game!" onPress={() => navigate("Game", {content: CONTENT})} />
+                <TouchableOpacity
+                    style={styles.gameButton}
+                    onPress={() => navigate("Game", {content: CONTENT})}
+                >
+                    <Text style={styles.gameButtonText}>{this.state.buttonText}</Text>
+                </TouchableOpacity>
                 <AnimatedSprite
                     ref="catRef"
                     sprite={catSprite}
                     animationFrameIndex={catSprite.animationIndex(this.state.animationType)}
                     loopAnimation={true}
-                    coordinates={{top:100, left: 50}}
+                    coordinates={{top:catY, left: catXMin}}
                     size={catSprite.size}
                     onPress={() => {this.onPress();}}
                     tweenOptions ={{
                         tweenType: "sine-wave",
-                        startXY: [50, 100],
-                        xTo: [100, 150, 100, 50],
-                        yTo: [100, 100, 100, 100],
-                        duration: 1000,
-                        loop: true
+                        startXY: [catXMin, catY],
+                        xTo: [catXMin, catXMax, catXMin],
+                        yTo: [catY],
+                        duration: 1500,
+                        loop: false
                     }}
                     tweenStart="fromMount"
-                />
-                <AnimatedSprite
-                    ref="catRef2"
-                    sprite={catSprite}
-                    animationFrameIndex={catSprite.animationIndex("ANGRY")}
-                    loopAnimation={true}
-                    coordinates={{top:300, left: 50}}
-                    size={catSprite.size}
-                    draggable={true}
                 />
             </View>
         );
@@ -113,9 +116,20 @@ class Introduction extends Component {
 
 const styles = StyleSheet.create({
     welcome: {
-        fontSize: 20,
-        textAlign: "center",
+        fontSize: 30,
+        textAlign: "left",
         margin: 10,
+    },
+    gameButton: {
+        height: Dimensions.get("window").height - catSprite.size.height - 50,
+        backgroundColor: "#e67e35",
+        justifyContent: "center"
+    },
+    gameButtonText: {
+        color: "#FFF",
+        fontSize: 50,
+        textAlign: "center",
+        fontWeight: "bold",
     }
 });
 
